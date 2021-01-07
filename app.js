6c5cbda58962f43c8357adb9de669df02a80f060
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2021-01-06 22:08:17 -0600

    Vue Basics Part 2
    - this code includes concepts from lectures 16-19
    - I learnt about methods in vue: very straightforward
    - I also learnt on how to access my data properties wherever in my app using the this keyword
    - I also learnt about the v-html directive. no code was written for this as i will rarely use this, if at all

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: "Apprendre Vue et Refiner Français",
+      courseGoalA: "Apprendre Vue et Refiner Français",
+      courseGoalB: "Apprendre Allemand",
       courseLink: "https://v3.vuejs.org/",
     };
   },
+  // NEW: methods. on y va
+  methods: {
+    outputGoal() {
+      const randomChiffre = Math.floor(Math.random() * 10);
+      console.log(randomChiffre);
+      return randomChiffre < 5 ? "Apprendre Vue" : "Maîtriser Vue";
+    },
+    outputGoal2() {
+      // NEW: Max is talking further about data, specifically this
+      // NEW: "vue takes all the data we return and merges it to our global vue instance object". this is why this.propertyName works
+      const randomChiffre = Math.floor(Math.random() * 50);
+      console.log(randomChiffre);
+      return randomChiffre < 25 ? this.courseGoalA : this.courseGoalB;
+    },
+    // my reliable reload page function is backk
+    reloadPage() {
+      window.location.reload();
+    },
+  },
 });
 
-// mounting our app to a unique selector in our HTML.
-// usually, in general, this is pretty global. But we're doing this this way to better learn Vue
 app.mount("#user-goal");
