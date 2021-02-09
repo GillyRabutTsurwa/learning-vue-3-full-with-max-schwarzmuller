df608bb2a74d92e3a1e69332660988c7891fe2e1
GillyRabutTsurwa <tsurwagilbert@gmail.com>
2021-02-08 21:11:29 -0600

    Vue Basics Part 3 - this code includes concepts from lectures 20-22 - started learning about event binding - we also learnt how to use methods in conjunction with events - for now, just the click events. more to come later

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoalA: "Apprendre Vue et Refiner Français",
-      courseGoalB: "Apprendre Allemand",
-      courseLink: "https://v3.vuejs.org/",
+      counter: 0, // ceci est utilisé directement sous le template HTML
+      counter2: 0, // ceci est utilisé ici sur le Javascript
+      counter3: 0,
+      incrementer: 15,
     };
   },
-  // NEW: methods. on y va
   methods: {
-    outputGoal() {
-      const randomChiffre = Math.floor(Math.random() * 10);
-      console.log(randomChiffre);
-      return randomChiffre < 5 ? "Apprendre Vue" : "Maîtriser Vue";
+    increment() {
+      this.counter2++;
     },
-    outputGoal2() {
-      // NEW: Max is talking further about data, specifically this
-      // NEW: "vue takes all the data we return and merges it to our global vue instance object". this is why this.propertyName works
-      const randomChiffre = Math.floor(Math.random() * 50);
-      console.log(randomChiffre);
-      return randomChiffre < 25 ? this.courseGoalA : this.courseGoalB;
+    decrement() {
+      this.counter2--;
     },
-    // my reliable reload page function is backk
+    fluidIncrement(num) {
+      this.counter3 = this.counter3 + num;
+    },
+    fluidDecrement(num) {
+      this.counter3 = this.counter3 - num;
+    },
     reloadPage() {
       window.location.reload();
     },
   },
 });
 
-app.mount("#user-goal");
+app.mount("#events");
--- notes.md
+++ notes.md
@@ -1,5 +1,5 @@
 ## Ce que l'on a appris
-* Creating a Vue instance and mounting it to a unique HTML selector.
-  * This really helped clear up how a Vue app is made
-* Interpolation and data binding
-* Makind attributes dynamic with the **v-bind** directive
+* Max went through how powerful Vue is and what it does behind the scenes par rapport au l'attachement de l'evenement (event binding)
+* These are lesssons 20-22
+* We learnt about events (for now, just the click event, similar to the click event listener in javascript)
+* we learnt how to add logic inline (not preferred) and how to make methods that call our button logic once clicked (preffered and imporant concept).
